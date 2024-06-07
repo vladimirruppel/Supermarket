dfc9e33ee37654937e6109165e4d6ce200d1d7b7
vladimirruppel <minecraft20044@mail.ru>
2024-06-07 18:45:24 +0700

    Улучшение скрипта передвижения камеры: обеспечена бесконечная генерация пола

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,12 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public GameObject cubePrefab;
-    public int startWidth = 30;
-    public int startHeight = 40;
+    public GameObject floorElementPrefab;
 
     public float panSpeed = 2f;
     public float panBorderThickness = 20f;
@@ -17,23 +16,65 @@ public class CameraMovement : MonoBehaviour
     public float minZoomHeight = 5f;
     public float maxZoomHeight = 20f;
 
+    public float borderCheckInterval = 1f;
+
     private Vector3 lastMousePosition;
+    private HashSet<GameObject> floorElements;
+    private Camera camera;
 
     void Start()
     {
-        int horizontalBorder = startWidth / 2;
-        int verticalBorder = startHeight / 2;
-
-        for (int i = -horizontalBorder; i < horizontalBorder; i++)
-        {
-            for (int j = -verticalBorder; j < verticalBorder; j++)
-            {
-                Instantiate(cubePrefab, new Vector3(i, 0, j), Quaternion.identity);
-            }
-        }
+        floorElements = new HashSet<GameObject>();
+        camera = GetComponent<Camera>();
     }
 
     void Update()
+    {
+        HashSet<GameObject> floorElementsOnScreen = new HashSet<GameObject>();
+
+        for (float x = 0; x <= Screen.width; x += borderCheckInterval)
+        {
+            for (float y = 0; y <= Screen.height; y += borderCheckInterval)
+            {
+                Vector3 screenPosition = new Vector3(x, y, 0);
+                Vector3 worldPosition = camera.ScreenToWorldPoint(screenPosition);
+                worldPosition.y = 0;
+
+                GameObject floorElement = IsFloorElementAlreadyInstantiated(worldPosition);
+                if (floorElement == null)
+                {
+                    Vector3 instantiatingPos = worldPosition;
+
+                    instantiatingPos.x = (int)Math.Round(worldPosition.x, MidpointRounding.AwayFromZero);
+                    instantiatingPos.z = (int)Math.Round(worldPosition.z, MidpointRounding.AwayFromZero);
+
+                    floorElement = InstantiateFloorElement(instantiatingPos);
+                }
+
+                floorElementsOnScreen.Add(floorElement);
+            }
+        }
+
+        HashSet<GameObject> objectToDelete = new HashSet<GameObject>();
+
+        foreach (GameObject obj in floorElements)
+        {
+            if (obj.GetComponent<FloorElement>().deleteIfNotVisible && !floorElementsOnScreen.Contains(obj))
+            {
+                objectToDelete.Add(obj);
+            }
+        }
+        
+        foreach (GameObject obj in objectToDelete)
+        {
+            floorElements.Remove(obj);
+            Destroy(obj);
+        }
+
+        objectToDelete.Clear();
+    }
+
+    void LateUpdate()
     {
         Vector3 pos = transform.position;
 
@@ -64,8 +105,8 @@ public class CameraMovement : MonoBehaviour
         if (Input.GetMouseButton(2))
         {
             Vector3 delta = Input.mousePosition - lastMousePosition;
-            pos.x -= delta.x * panSpeed * Time.deltaTime;
-            pos.z -= delta.y * panSpeed * Time.deltaTime;
+            pos.x -= delta.x * panSpeed * Time.deltaTime * pos.y;
+            pos.z -= delta.y * panSpeed * Time.deltaTime * pos.y;
             lastMousePosition = Input.mousePosition;
         }
 
@@ -78,5 +119,34 @@ public class CameraMovement : MonoBehaviour
         }
 
         transform.position = pos;
+    }
+
+    private GameObject InstantiateFloorElement(Vector3 pos)
+    {
+        GameObject instantiatedObject = Instantiate(floorElementPrefab, pos, Quaternion.identity);
+        floorElements.Add(instantiatedObject);
+        return instantiatedObject;
+    }
+
+    private GameObject IsFloorElementAlreadyInstantiated(Vector3 position)
+    {
+        foreach (GameObject floorElement in floorElements)
+        {
+            Vector3 floorPosition = floorElement.transform.position;
+            if (IsPositionInsideFloorElement(floorPosition, position))
+            {
+                return floorElement;
+            }
+        }
+        return null;
+    }
+
+    // Helper method to check if a position is inside a cube at cubePosition
+    private bool IsPositionInsideFloorElement(Vector3 objectPosition, Vector3 position)
+    {
+        float cubeHalfSize = 0.5f; // Since the cube dimensions are 1x1x1, half-size is 0.5
+        return position.x >= objectPosition.x - cubeHalfSize && position.x <= objectPosition.x + cubeHalfSize &&
+               position.y >= objectPosition.y - cubeHalfSize && position.y <= objectPosition.y + cubeHalfSize &&
+               position.z >= objectPosition.z - cubeHalfSize && position.z <= objectPosition.z + cubeHalfSize;
     }
 }
---
+++ Assets/Scripts/FloorElement.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorElement : MonoBehaviour
+{
+    public bool deleteIfNotVisible = true;
+}
