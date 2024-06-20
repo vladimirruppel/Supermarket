2fce4a0f13ebe34600a834a1ec44787e9bfa25ff
vladimirruppel <minecraft20044@mail.ru>
2024-06-20 20:52:34 +0700

    Создание UI для взаимодействия пользователя с магазином (границы, прилавки, продукты, кассы)

---
+++ Assets/Scripts/UIToolsMenuScript.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIToolsMenuScript : MonoBehaviour
+{
+    private UIDocument document;
+
+    private List<Button> actionButtons = new List<Button>();
+
+    private const string addWallsButtonName = "AddWallsButton";
+    private const string removeWallsButtonName = "RemoveWallsButton";
+    private const string addEnterDoorsButtonName = "AddEnterDoorsButton";
+    private const string addWarehouseDoorsButtonName = "AddWarehouseDoorsButton";
+    private const string removeDoorsButtonName = "RemoveDoorsButton";
+    private const string changeBordersConfirmButtonName = "ChangeBordersConfirmButton";
+    private const string changeBordersCancelButtonName = "ChangeBordersCancelButton";
+    private const string addCashDesksButtonName = "AddCashDesksButton";
+    private const string removeCashDesksButtonName = "RemoveCashDesksButton";
+    private const string getInformationButtonName = "GetInformationButton";
+    
+    void Awake()
+    {
+        document = GetComponent<UIDocument>();
+
+        actionButtons = document.rootVisualElement.Query<Button>(className: "button").ToList();
+        int actionButtonsLength = actionButtons.Count;
+        for (int i = 0; i < actionButtonsLength; i++) {
+            actionButtons[i].RegisterCallback<ClickEvent, Button>(ActionButtonClickHandler, actionButtons[i]);
+        }
+    }
+
+    void OnDisable()
+    {
+        int actionButtonsLength = actionButtons.Count;
+        for (int i = 0; i < actionButtonsLength; i++)
+        {
+            actionButtons[i].UnregisterCallback<ClickEvent, Button>(ActionButtonClickHandler);
+        }
+    }
+
+    private void ActionButtonClickHandler(ClickEvent evt, Button clickedButton) {
+        if (clickedButton.ClassListContains("menu-button")) {
+            VisualElement menuParent = clickedButton.parent;
+
+            if (menuParent.ClassListContains("active"))
+            {
+                menuParent.RemoveFromClassList("active");
+
+                List<VisualElement> activeChildMenus = menuParent.Query(className: "menu").Where(menu => menu.ClassListContains("active")).ToList();
+                int activeChildMenusCount = activeChildMenus.Count;
+                for (int i = 0; i < activeChildMenusCount; i++) {
+                    VisualElement currentActiveChildMenu = activeChildMenus[i];
+                    currentActiveChildMenu.RemoveFromClassList("active");
+
+                    // если это не корневой элемент-меню
+                    if (currentActiveChildMenu.Query(className: "menu").Where(menu => menu.ClassListContains("active")).ToList().Count > 0) {
+                        break;
+                    }
+
+                    // действия если это корневой элемент-меню
+                    // возможно тут должна быть проверка на имя активной кнопки
+                    // и в зависимости от имени кнопки отменяется ее действие
+                }
+            }
+            else
+            {
+                List<VisualElement> activeMenus = document.rootVisualElement.Query(className: "menu").Where(menu => menu.ClassListContains("active")).ToList();
+                int activeMenusCount = activeMenus.Count;
+                for (int i = 0; i < activeMenusCount; i++) {
+                    if (activeMenus[i].name != menuParent.parent.parent.name)
+                        activeMenus[i].RemoveFromClassList("active");
+                }
+
+                menuParent.AddToClassList("active");
+            }
+        }
+
+        switch (clickedButton.name) {
+            default:
+                break;
+        }
+    }
+}
